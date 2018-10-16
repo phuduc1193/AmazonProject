a72f0962076c8f5303d2f38fad6ac3653b4f321b
Duc Truong <phuduc1193@gmail.com>
2018-10-16 01:23:59 -0500

    add seller background

--- AmazonProjectAngular/ClientApp/src/app/views/seller/seller.module.ts
+++ AmazonProjectAngular/ClientApp/src/app/views/seller/seller.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "@angular/common";
 
 import { SellerRoutingModule } from "./seller-routing.module";
 import { SellerComponent } from "./seller.component";
+import { SellerLayoutComponent } from "./seller-layout/seller-layout.component";
+import { SellerHeaderComponent } from "./seller-header/seller-header.component";
 
 @NgModule({
   imports: [CommonModule, SellerRoutingModule],
-  declarations: [SellerComponent]
+  declarations: [SellerComponent, SellerLayoutComponent, SellerHeaderComponent]
 })
 export class SellerModule {
   constructor() {
