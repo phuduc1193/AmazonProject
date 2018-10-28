b6edd1f81ae94800f4742c2063102b1114806266
ttngu207 <thinhnguyen0405@gmail.com>
2018-10-28 17:20:55 -0500

    1st skeleton for cart service

---
+++ Service/Cart/CartService/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartService.Models
+{
+    public class Cart
+    {
+        public string CartId { get; set; }
+        public string UserId { get; set; }
+        public DateTime DateLastModified { get; set; }
+        public enum CartStatus
+        {
+            Current, // This cart is the current cart that this user is shopping on
+            Ordered, // This cart has been ordered
+            Deleted, // This cart has been deleted
+            Saved // This cart has been saved by the user and he/she has moved on to new cart
+        }
+
+        public List<CartItem> CartItems { get; set; }
+
+    }
+}
---
+++ Service/Cart/CartService/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartService.Models
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
