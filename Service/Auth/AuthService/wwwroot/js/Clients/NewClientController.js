ef7594ad1d2f315d50eaf412d40489c5d47cf93c
Duc Truong <phuduc1193@gmail.com>
2018-11-09 01:10:10 -0600

    add information fields for client setup

--- Service/Auth/AuthService/wwwroot/js/Clients/NewClientController.js
+++ Service/Auth/AuthService/wwwroot/js/Clients/NewClientController.js
@@ -1,75 +1,67 @@
 ﻿angular.module('authServiceApp', [])
     .controller('NewClientController', ['$http', '$scope', function ($http, $scope) {
-
         $scope.activeTabIndex = 0;
         $scope.progression = 0;
 
-        $scope.activeTab = function (index) {
-            $scope.activeTabIndex = index;
-            $scope.progression = index;
-        }
-
-        $scope.getBreadcrumFieldClass = function (index) {
-            return { 'active': $scope.activeTabIndex == index, 'completed': $scope.progression > index && $scope.activeTabIndex != index };
-        }
-
-        const defaultScope = {
+        $scope.client = {
             Name: "",
             DisplayName: "",
             Description: "",
-            Required: true,
-            Emphasize: true
-        };
-        var api = this;
-        api.Name = "";
-        api.DisplayName = "";
-        api.Description = "";
-        api.Enabled = true;
-        api.Scopes = [angular.copy(defaultScope)];
-        api.Secrets = [];
-
-        api.addScope = function () {
-            api.Scopes.push(angular.copy(defaultScope));
+            AllowedGrantTypes: null,
+            Secrets: [],
         };
 
-        api.removeScope = function (index) {
-            api.Scopes.splice(index, 1);
+        $scope.activeTab = function (event, index) {
+            if (index > $scope.progression) {
+                event.preventDefault();
+                event.stopPropagation();
+                return;
+            }
+            $scope.activeTabIndex = index;
         }
 
-        api.create = function () {
-            $http.post('New', api, {
-                headers: {
-                    'X-XSRF-TOKEN': $('#RequestVerificationToken').val()
-                }
-            }).then(function () {
-                $("#cancel").get(0).click();
-            }, function () {
-                bootbox.alert({
-                    message: '<div class="py-3">An unexpected error has occurred. Please try again!</div>',
-                    callback: function () {
-                        location.reload();
-                    },
-                    buttons: {
-                        ok: {
-                            label: 'Try again',
-                            className: 'btn btn-outline-danger'
-                        }
-                    }
-                });
-            });
-        };
+        $scope.shouldAdvanceInfo = function (e) {
+            e.preventDefault();
+            if ($scope.client.AllowedGrantTypes)
+                $('#info-tab').get(0).click();
+        }
 
-        api.generateSecret = function () {
-            api.Secrets = [];
+        $scope.shouldAdvanceScope = function (e) {
+            e.preventDefault();
+            if (!$scope.client.Name || !$scope.client.DisplayName || !$scope.client.Description)
+                return;
+            if ($scope.client.AllowedGrantTypes != 'Implicit' && $scope.client.Secrets.length == 0)
+                return;
+            if ($scope.client.AllowedGrantTypes != 'ClientCredentials' && !$scope.client.RedirectUris)
+                return;
+
+            if ($scope.progression < 2)
+                $scope.progression = 2;
+            $('#scope-tab').get(0).click();
+        }
+
+        $scope.getBreadcrumFieldClass = function (index) {
+            return { 'active': $scope.activeTabIndex == index, 'completed': $scope.progression > index && $scope.activeTabIndex != index, 'muted not-allowed': $scope.progression < index };
+        }
+
+        $scope.setAllowedGrantTypes = function (type) {
+            $scope.client.AllowedGrantTypes = type;
+            if ($scope.progression < 1)
+                $scope.progression = 1;
+        }
+
+        $scope.generateSecret = function (e) {
+            e.preventDefault();
+            $scope.client.Secrets = [];
             const secretString = sha256(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5));
             const secretData = {
                 Value: secretString,
-                Description: `Secret for ${api.Name}`
+                Description: `Secret for ${$scope.client.Name}`
             };
-            api.Secrets.push(secretData);
+            $scope.client.Secrets.push(secretData);
         }
 
-        api.clipboardText = function (str) {
+        $scope.clipboardText = function (str) {
             const el = document.createElement('textarea');
             el.value = str;
             document.body.appendChild(el);
